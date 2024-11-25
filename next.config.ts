52142d1b7bbf6c45a2d37b03f7e15d9570093db2
Sahag Arzoumanian <arzsahag@gmail.com>
2024-11-24 18:17:46 -0800

    Change next.config to js

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-      },
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
